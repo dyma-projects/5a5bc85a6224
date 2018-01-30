75ca02c55de92a701f64cd08aaeafc826defdb7a
Mayeul <mayeulderouge@gmail.com>
2018-01-30 01:15:25 +0100

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
+  compteurEnfant : number = 0;
+  @Output() changementValeurCompteur = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  augmenter(){
+  	this.compteurEnfant++;
+  	this.changementValeurCompteur.emit({
+  		value: this.compteurEnfant
+  	});
+  } 
+  diminuer(){
+  	this.compteurEnfant--;
+  	this.changementValeurCompteur.emit({
+  		value: this.compteurEnfant
+  	});
   }
 
 }
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice2.component.css']
 })
 export class Exercice2Component implements OnInit {
+  
+  @ViewChild('el') public el: ElementRef;
   public valeur: string;
-
-  constructor() { }
+	
+  constructor() { 
+  }
 
   ngOnInit() {
   }
+
+  afficher(){
+  	this.valeur=this.el.nativeElement.value;
+  	console.log('value='+this.el.nativeElement.value);
+  }
+ 
 
 }
